1fdd01606e70bbaf6e915cf886e2cdd2625b1bd6
Cloud User <centos@ip-172-31-31-56.ec2.internal>
2018-05-01 18:52:37 +0000

    updates made so terraform does configuration

--- packer/setup_openvpn.sh
+++ packer/setup_openvpn.sh
@@ -24,36 +24,4 @@ sudo ./pkitool --initca
 sudo ./pkitool --server server
 sudo ./build-dh
 
-  INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-  AWS_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region'`
-
-  echo $INSTANCE_ID
-
-#pull private dns name to use in update
-  PRIVATEDNSIP=`aws ec2 describe-instances --instance-id $INSTANCE_ID --region $AWS_REGION | jq .Reservations[].Instances[].PrivateIpAddress |sed 's/\"//g'`
-
-  echo $PRIVATEDNSIP
-  sed -i -e 's/local_IP/'$PRIVATEDNSIP'/g' /tmp/aws.server.conf
-
-  MASTER_INSTANCE_ID=`aws --output text --region $AWS_REGION ec2 describe-tags --filters 'Name=tag:Type,Values=dns_master' | awk '{print $3}'`
-  MASTER_SERVER_IP=`aws ec2 describe-instances --instance-id $MASTER_INSTANCE_ID --region $AWS_REGION | jq .Reservations[].Instances[].PublicIpAddress |sed 's/\"//g'`
-
-  sed -i -e 's/master_DNS_IP/'$MASTER_SERVER_IP'/g' /tmp/aws.server.conf
-
-  SLAVE_INSTANCE_ID=`aws --output text --region $AWS_REGION ec2 describe-tags --filters 'Name=tag:Type,Values=dns_slave' | awk '{print $3}'`
-  SLAVE_SERVER_IP=`aws ec2 describe-instances --instance-id $SLAVE_INSTANCE_ID --region $AWS_REGION | jq .Reservations[].Instances[].PublicIpAddress |sed 's/\"//g'`
-
-  sed -i -e 's/slave_DNS_IP/'$SLAVE_SERVER_IP'/g' /tmp/aws.server.conf
-  VPC_CIDRS=`aws ec2 describe-vpcs  --region $AWS_REGION --vpc-id $VPC_ID |  jq .Vpcs[].CidrBlockAssociationSet[].CidrBlock |sed 's/\"//g' | awk -F/ '{print $1}'`
-  echo VPC_CIDRS $VPC_CIDRS
-  for VPC_CIDR in $VPC_CIDRS
-    do
-      VPC_UPDATE+='push_"route_'$VPC_CIDR'_255.255.255.0"\n'
-    done
-  sed -i '/vpc_cidr/c '$VPC_UPDATE'' /tmp/aws.server.conf
-  sed -i -e 's/_/ /g' /tmp/aws.server.conf
-
-  sudo cp /tmp/aws.server.conf /etc/openvpn/server/aws.server.conf
-
 sudo systemctl enable openvpn
-sudo systemctl start openvpn
