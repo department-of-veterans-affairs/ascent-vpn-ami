756e27a130ee773b882d5936b060fa9973e1c1fa
jasonluck <jluck@meetveracity.com>
2018-05-22 11:31:06 -0400

    outputing private ip

--- modules/vpn-server/outputs.tf
+++ modules/vpn-server/outputs.tf
@@ -2,6 +2,10 @@ output "vpn_eip" {
   value = "${aws_instance.vpn.public_ip}"
 }
 
+output "private_ip" {
+  value = "${aws_instance.vpn.private_ip}"
+}
+
 output "security_group_id" {
   value = "${aws_security_group.vpn_security_group.id}"
 }
